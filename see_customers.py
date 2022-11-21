8789c4f6b09f6f59ecc8dfdc2cffda01d5906ac3
AksshayaR <78347814+AksshayaR@users.noreply.github.com>
2022-11-21 20:29:00 +0530

    Update see_customers.py

--- see_customers.py
+++ see_customers.py
@@ -10,9 +10,4 @@ def read():
     df = pd.DataFrame(result, columns=['c_id', 'cust_name', 'cust_mobile', 'cust_email', 'check_in', 'check_out'])
     with st.expander("View all Customers"):
         st.dataframe(df)
-    '''with st.expander("Room Details"):
-        train_df = df['Availability'].value_counts().to_frame()
-        train_df = train_df.reset_index()
-        st.dataframe(train_df)
-        p1 = px.pie(train_df, names='index', values='Availability')
-        st.plotly_chart(p1)'''
+    
