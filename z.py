2f6a61ffddc37e0d2dc6ceb78a1b3c5f8303469f
AksshayaR <78347814+AksshayaR@users.noreply.github.com>
2022-11-27 20:47:38 +0530

    Delete z.py

--- z.py
+++
@@ -1,6 +0,0 @@
-from kazoo.client import KazooClient
-from kazoo.handlers.threading import SequentialThreadingHandler
-zk = KazooClient(hosts="localhost:2181",handler=SequentialThreadingHandler())
-zk.start()
-children = zk.get_children('/')
-zk.stop()
